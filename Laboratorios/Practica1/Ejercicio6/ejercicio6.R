c256815760d84c4e6eaf7bfbe7dbdd7df776dbf3
mleon <narolr@hotmail.com>
2018-05-28 10:48:54 -0500

    Subiendo ejercicio6.R incompleto.

---
+++ Laboratorios/Practica1/Ejercicio6/ejercicio6.R
@@ -0,0 +1,20 @@
+# Nombre: Marco Naro León Rios 20160775E
+# Pregunta 6:
+#veamos si podemos acceder a datasets
+help("datasets")
+library(help = "datasets")
+quakes
+archivo <- subset(quakes, subset = quakes$mag >= 5)
+# creamos la carpeta contenedor para almacenar el archivo
+system("mkdir contenedor")
+# almacenamos el archivo en la carpeta contenedora
+write.table(archivo, file = "./contenedor/q5.txt", append = FALSE,
+            sep = "!", row.names = FALSE)
+# ahora importamos el mismo archivo
+q5.dframe <-read.table("./contenedor/q5.txt", sep = "!", header = TRUE)
+# no puedo activar el paquete car....
+
+
+
+
+#script inconcluso
