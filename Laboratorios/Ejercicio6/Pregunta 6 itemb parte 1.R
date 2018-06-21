23963c5ef72f6096bc5ba582e29c7e4254134865
mleon <narolr@hotmail.com>
2018-06-21 11:47:18 -0500

    Se eliminó, inevitablemente, lo inecesario y repetido.

--- Laboratorios/Ejercicio6/Pregunta 6 itemb parte 1.R
+++
@@ -1,6 +0,0 @@
-## item a ##
-
-qux <- array(96:1,dim=c(4,4,2,3))   ## se inicializa el array
-z=apply(qux[,,2,],3,diag)    ## bucle implicito mediante la funcion apply
-  ##qux[,,2,] es para trabajar sobre la segunda capa,MARGIN=3 es para trabajar sobre la dimensión 3, "diag" es el operador diagonal de la matriz  
-z ##visualizamos el resultado
