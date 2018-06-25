39c43dbf36c0cde27b6588819c62584b9b503f2f
Victor-Ponce-Pinedo <vipopi1997@gmail.com>
2018-06-24 19:32:31 -0500

    Refinando pregunta 1

--- Laboratorios/Laboratorio2/Ejercicio1/Pregunta1.R
+++ Laboratorios/Laboratorio2/Ejercicio1/Pregunta1.R
@@ -2,13 +2,15 @@
 
 # Respuesta 1.a: El codigo muestra el uso de la funcion "ls"
 # para listar el paquete methods.
-arr=ls(package:methods)
-# Mostrar la cantidad de elementos.
+# al hacer all.names=TRUE no se omitiran los elementos de METHODS que empiezen con el caracter "."
+arr=ls("package:methods",all.names=TRUE)
+# Mostrar los elementos.
 arr
 # Mostrar la cantidad de elementos.
 length(arr)
 # Modificamos arr
 arr = arr[1:20]
+# Mostramos los 20 primeros elementos
 print(arr)
 
 # Respuesta 1.b: El codigo muestra el uso de la funcion "environment"
@@ -21,7 +23,7 @@ environment(jpeg)
 
 #Respuesta 1.c: El codigo muestra el uso de la funcion "which"
 # para comprobar que un paquete esta dentro de otro
-x=ls(package:graphics)
+x=ls("package:graphics")
 x
 # Nos muestra indice el indice del paquete "smoothScatter"
 # dentro del paquete "graphics
